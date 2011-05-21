6e7623623f3aba69fed02f657ff3ebabd4fca580
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-05-21 00:27:28 -0300

    removendo TODOs desnecessarios...

--- Loja/Loja-ejb/src/java/com/geekvigarista/pojo/Compra.java
+++ Loja/Loja-ejb/src/java/com/geekvigarista/pojo/Compra.java
@@ -74,7 +74,6 @@ public class Compra implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Compra)) {
             return false;
         }
