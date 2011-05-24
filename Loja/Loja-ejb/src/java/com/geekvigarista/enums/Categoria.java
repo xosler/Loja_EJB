e0a62f7f1e0d40a5f21b36e5f1c8bd8c56f84c1d
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-05-23 23:50:29 -0300

    adicionando categoria.. (ENUM)

--- Loja/Loja-ejb/src/java/com/geekvigarista/enums/Categoria.java
+++ Loja/Loja-ejb/src/java/com/geekvigarista/enums/Categoria.java
@@ -9,29 +9,20 @@ package com.geekvigarista.enums;
  * @author carlos
  */
 public enum Categoria {
-    AVENTAL(1, "Avental"), 
-    BLUSA(2, "Blusa"), 
-    CAMISETA(3, "Camiseta"), 
-    CAMISA(4, "Camisa"), 
-    CALCA(5, "Calça"), 
-    JEANS(6, "Jeans"), 
-    MOLETON(7, "Moleton"), 
-    CASACO(8, "Casaco"), 
-    JAQUETA(9, "Jaqueta"), 
-    TERNO(10, "Terno"), 
-    PALETO(11, "Paletó");
+    ADORNO("Adornos"),
+    BLUSA("Blusas"), 
+    CAMISETA("Camisetas"), 
+    CAMISA("Camisas"),
+    CALCA("Calças"), 
+    CASACO("Casacos"), 
+    JAQUETA("Jaquetas"),
+    OUTRO("Outros");
     
-    private final int id;
     private final String nome;
     
-    private Categoria(int id, String nome)
+    private Categoria(String nome)
     {
-        this.id = id;
         this.nome = nome;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getNome() {
