6741aad4dcc29b834ef4939cb6580392745f8fc3
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-05-26 22:51:42 -0300

    melhorado esquema de categoria

--- Loja-WEB/src/java/com/geekvigarista/converter/CategoriaConverter.java
+++ Loja-WEB/src/java/com/geekvigarista/converter/CategoriaConverter.java
@@ -8,7 +8,6 @@ import com.geekvigarista.pojo.Categoria;
 import com.geekvigarista.services.CategoriaServiceLocal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ejb.EJB;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -22,34 +21,25 @@ import javax.naming.NamingException;
  */
 @FacesConverter("categoriaConverter")
 public class CategoriaConverter implements Converter {
-    
+
     private CategoriaServiceLocal servico;
-    
+
+    public CategoriaConverter() throws NamingException {
+        InitialContext ic = new InitialContext();
+        servico = (CategoriaServiceLocal) ic.lookup("java:global/Loja/Loja-ejb/CategoriaService");
+    }
+
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        try {
-            System.out.print("aqw " + value);
-            
-            InitialContext ic = new InitialContext();
-            servico = (CategoriaServiceLocal) ic.lookup("java:global/Loja/Loja-ejb/CategoriaService");
-            return servico.find(new Long(value));
-        } catch (NamingException ex) {
-            Logger.getLogger(CategoriaConverter.class.getName()).log(Level.SEVERE, null, ex);
-            
-        }
-        return null;
+        return servico.find(new Long(value));
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        
-        System.out.println(value.getClass());
-        
         if (value instanceof Categoria) {
             return ((Categoria) value).getId().toString();
         } else {
             throw new IllegalArgumentException("tá errado essa merda");
-
         }
     }
 }
