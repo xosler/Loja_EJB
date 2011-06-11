66b36d88eaff245c046369a496d5435b23017660
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-06-11 00:17:44 -0300

    tentando arrumar problemas com forms (sempre); abusando o safado do CDI do JSF e removendo TUDO da sessão quando faz logout :)

--- Loja-WEB/src/java/com/geekvigarista/manageds/ManagedLogin.java
+++ Loja-WEB/src/java/com/geekvigarista/manageds/ManagedLogin.java
@@ -10,8 +10,8 @@ import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -20,7 +20,7 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class ManagedLogin extends ManagedCadastro implements Serializable {
-
+    
     private Usuario logado = null;
     private String user;
     private String password;
@@ -33,30 +33,30 @@ public class ManagedLogin extends ManagedCadastro implements Serializable {
                 return true;
             }
         }
-
+        
         return false;
     }
-
+    
     public Usuario getLogado() {
         return logado;
     }
-
+    
     public void setLogado(Usuario logado) {
         this.logado = logado;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public String getUser() {
         return user;
     }
-
+    
     public void setUser(String user) {
         this.user = user;
     }
@@ -64,7 +64,7 @@ public class ManagedLogin extends ManagedCadastro implements Serializable {
     /** Creates a new instance of ManagedLogin */
     public ManagedLogin() {
     }
-
+    
     public String login() {
         Usuario u = service.login(user, password);
         if (u != null && u.getId() != null) {
@@ -75,13 +75,20 @@ public class ManagedLogin extends ManagedCadastro implements Serializable {
             return "erro";
         }
     }
-
+    
     public String cadastrar() {
         return "cadastrar";
     }
-
+    
     public String logout() {
         logado = null;
+
+        // ao sair, também tenho que zerar os manageds, né?
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("managedUsuario", null);        
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("managedCarrinho", null);        
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("managedProduto", null);        
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("managedCompra", null);        
+        
         return "sair";
     }
 }
